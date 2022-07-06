6af02b7d0393ca7ad6fa4a8daf1c048de173df9e
karan51ngh <karansingh9535@gmail.com>
2022-07-06 23:58:49 +0530

    Made CHanges to the GUI

--- doctracerGUI/main.py
+++ doctracerGUI/main.py
@@ -1,35 +1,60 @@
-import os
 import streamlit as st
+import os
 
-li=[]
-
-def func(path,tar, print=print):
-	
-	dir_list=os.listdir(path)
-	for i in dir_list:
-		pathx=path
-		pathx=pathx+'/'+i
-		if os.path.isdir(pathx):
-			func(pathx,tar,print=print)
-		if os.path.isfile(pathx):
-			if i == tar:
-				print("FOUND!")
-				print("location:"+pathx)
-				li.append(pathx)
-			else:
-				continue
-
-	
-
-#path=st.text_input("enter the location of the root folder",value='/home/osboxes/defenceForces/army/1.officers')
-#tar=st.text_input("enter the name of the file",value='ltKaran.txt')
-path=st.text_input("enter the location of the root folder")
-tar=st.text_input("enter the name of the file")
+st.set_page_config(
+    page_title="My Portfolio",
+    # page_icon="pics/icon.png",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Get Help': None,
+        'Report a bug': None,
+        'About': None
+    }
+)
+hide_menu_style = """
+        <style>
+        #MainMenu {visibility: hidden; }
+        footer {visibility: hidden;}
+        </style>
+        """
+st.markdown(hide_menu_style, unsafe_allow_html=True)
+li = []
 
 
-#flag = func(path,tar,print=st.write)
+def func(path, tar):
 
-func(path,tar,print=st.write)
+    dir_list = os.listdir(path)
+    temp = False
+    for i in dir_list:
+        pathx = path
+        pathx = pathx+'/'+i
+        if os.path.isdir(pathx):
+            temp = temp or func(pathx, tar)
+        if os.path.isfile(pathx):
+            if i == tar:
+                st.success("FOUND!")
+                st.info("Location of the file:"+pathx)
+                li.append(pathx)
+                return True
+            else:
+                continue
+    return temp
+# path=st.text_input("enter the location of the root folder",value='/home/osboxes/defenceForces/army/1.officers')
+# tar=st.text_input("enter the name of the file",value='ltKaran.txt')
+# path = st.text_input("enter the location of the root folder")
+# tar = st.text_input("enter the name of the file")
 
-if not li:
-	st.warning("File not found!")
+
+with st.form("my_form2"):
+    path = st.text_input(
+        'Enter the location of the directory you want to explore')
+    tar = st.text_input('Enter the name of the file you want to search')
+    submitted = st.form_submit_button("Submit")
+    if submitted:
+        flag = func(path, tar)
+        if not flag:
+            st.warning("File not found!")
+
+
+# flag = func(path,tar,print=st.write)
